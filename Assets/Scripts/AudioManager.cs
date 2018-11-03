bc437975dca756d7f890aa930bac8a3b4b5415f3
Isaiah Mann <isaiahbmann@gmail.com>
2018-11-03 15:51:02 -0700

    Add audio manager with first track (#8)

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+    [SerializeField]
+    AudioSource musicChannel;
+
+    [SerializeField]
+    AudioClip sculptingClip;
+
+    void Start()
+    {
+        musicChannel.clip = sculptingClip;
+        musicChannel.Play();
+    }
+}
