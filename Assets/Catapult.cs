a5d734c126cc993553d06ddf09899240da25fed7
JXVD <jackvangame@live.com>
2018-11-03 11:56:04 -0700

    Minor changes

---
+++ Assets/Catapult.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Catapult : MonoBehaviour {
+    public float thrust;
+    public Rigidbody rb;
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+        
+        if(Input.GetMouseButtonDown(1))
+        {
+            rb.AddForce(transform.up * thrust);
+        }	
+	}
+}
