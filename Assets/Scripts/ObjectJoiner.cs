0406a8b9d1e649115eb0a1d5cdd48386da3c4371
Isaiah Mann <contact@isaiahmann.com>
2018-11-03 13:14:36 -0700

    Modify prefab physics

--- Assets/Scripts/ObjectJoiner.cs
+++ Assets/Scripts/ObjectJoiner.cs
@@ -1,14 +1,21 @@
 ﻿using UnityEngine;
 
-public class ObjectJoiner : MonoBehaviour {
-    public GameObject Join(string objectName, GameObject[] components, Vector3 center) {
+public class ObjectJoiner : MonoBehaviour
+{
+    public GameObject Join(string objectName, GameObject[] components, Vector3 center)
+    {
         GameObject parent = new GameObject(objectName);
         Transform parentT = parent.transform;
         parent.AddComponent<Rigidbody>();
         parentT.position = center;
-        foreach (GameObject comp in components) {
+        foreach (GameObject comp in components)
+        {
+            MeshFilter filter;
             FixedJoint parentJoint = parent.AddComponent<FixedJoint>();
-            parentJoint.connectedBody = comp.GetComponent<Rigidbody>();
+            Rigidbody compRigibody = comp.GetComponent<Rigidbody>();
+            compRigibody.isKinematic = false;
+            compRigibody.useGravity = true;
+            parentJoint.connectedBody = compRigibody;
             comp.transform.SetParent(parentT);
         }
         return parent;
