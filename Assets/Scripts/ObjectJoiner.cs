1de647c124311196a567ffaba9f81b871030911b
Isaiah Mann <contact@isaiahmann.com>
2018-11-03 12:38:42 -0700

    Add proof of concept object joining logic

---
+++ Assets/Scripts/ObjectJoiner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectJoiner : MonoBehaviour {
+    public GameObject Join(string objectName, GameObject[] components, Vector3 center) {
+        GameObject parent = new GameObject(objectName);
+        Transform parentT = parent.transform;
+        parent.AddComponent<Rigidbody>();
+        parentT.position = center;
+        foreach (GameObject comp in components) {
+            FixedJoint parentJoint = parent.AddComponent<FixedJoint>();
+            parentJoint.connectedBody = comp.GetComponent<Rigidbody>();
+            comp.transform.SetParent(parentT);
+        }
+        return parent;
+    }
+}
