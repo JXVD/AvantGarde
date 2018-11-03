c6d443f30099a17b33f8fcd64818dacdd097fae6
Isaiah Mann <contact@isaiahmann.com>
2018-11-03 13:06:00 -0700

    Update test scene with new asset

--- Assets/Scripts/ObjectJoiner.cs
+++ Assets/Scripts/ObjectJoiner.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ObjectJoiner : MonoBehaviour {
     public GameObject Join(string objectName, GameObject[] components, Vector3 center) {
