901138c7d9831aa0e2fa1eaa6432112f69e89990
Isaiah Mann <contact@isaiahmann.com>
2018-11-03 18:52:56 -0700

    Add Buy It Guy

---
+++ Assets/Scripts/TriggerOnStop.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class TriggerOnStop : MonoBehaviour {
+    Rigidbody physics;
+    float velocityTolerance = 0.01f;
+    Action onStop;
+    bool hasStopped = false;
+    [SerializeField]
+    bool debugEnabled = false;
+    bool isListening = true;
+
+    void Awake() {
+        physics = GetComponent<Rigidbody>();
+    }
+
+    public void ToggleListening(bool isListening) {
+        this.isListening = isListening;
+    }
+
+    public void SetVelocityTolerance(float tolerance) {
+        velocityTolerance = tolerance;
+    }
+
+    public void OnStop(Action action) {
+        onStop += action;
+    }
+
+    void Update() {
+        if (!isListening) return;
+
+        if (debugEnabled) Debug.Log(physics.velocity.magnitude);
+        if (!hasStopped && 
+            physics.velocity.magnitude <= velocityTolerance && 
+            onStop != null) {
+            onStop();
+        }
+    }
+}
